605f232f06241bd794f7f6be61f304e5a55aa3a6
Camilost1404 <camitorres1404@gmail.com>
2020-12-19 18:34:18 -0500

    Middleware terminado

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,18 +2,48 @@
 const tokenService = require('../services/token');
 
 module.exports = {
-    verifyUsuario: async(req, res, next) => {
+    verifyAdmin: async(req, res, next) => {
         if (!req.headers.token) {
             return res.status(404).send({
-                message: 'No token'
+                message: 'Token not found'
             });
         }
-        const response = await tokenService.decode(req.headers.token);
-        if (response.rol == 'Administrador' || response.rol == 'Vendedor' || response.rol == 'Almacenero') {
+        const responseValid = await tokenService.decode(req.headers.token);
+        if (responseValid.rol === 'Administrador') {
             next();
         } else {
             return res.status(403).send({
-                message: 'No autorizado'
+                message: 'Unauthorized'
+            });
+        }
+    },
+    verifySeller: async(req, res, next) => {
+        if (!req.headers.token) {
+            return res.status(404).send({
+                message: 'Token not found'
+            });
+        }
+        const responseValid = await tokenService.decode(req.headers.token);
+        if (responseValid.rol === 'Administrador' || responseValid.rol === 'Vendedor') {
+            next();
+        } else {
+            return res.status(403).send({
+                message: 'Unauthorized'
+            });
+        }
+    },
+    verifySK: async(req, res, next) => {
+        if (!req.headers.token) {
+            return res.status(404).send({
+                message: 'Token not found'
+            });
+        }
+        const responseValid = await tokenService.decode(req.headers.token);
+        if (responseValid.rol === 'Administrador' || responseValid.rol === 'Almacenero') {
+            next();
+        } else {
+            return res.status(403).send({
+                message: 'Unauthorized'
             });
         }
     },
