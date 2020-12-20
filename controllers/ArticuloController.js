753f6a02259212b497d803056dee05a2a6d02b39
Camilost1404 <camitorres1404@gmail.com>
2020-12-19 20:24:25 -0500

    Ultimos cambios

--- controllers/ArticuloController.js
+++ controllers/ArticuloController.js
@@ -2,7 +2,7 @@ const models = require("../models/index.js")
 
 exports.list = async(req, res, next) => {
     try {
-        const articles = await models.Articulo.findAll({
+        const articles = await models.Articulos.findAll({
             attributes:{
                 exclude: ['categoriaId', "createdAt", "updatedAt"],  
             },
@@ -25,7 +25,7 @@ exports.list = async(req, res, next) => {
 exports.add = async (req, res, next) => {
     try {
 
-        const article = await models.Articulo.create(req.body);
+        const article = await models.Articulos.create(req.body);
         res.status(200).json(article);
 
     } catch (error) {
@@ -39,7 +39,7 @@ exports.add = async (req, res, next) => {
 exports.update = async (req, res, next) => {
     try {
 
-        const article = await models.Articulo.update({codigo: req.body.codigo, nombre: req.body.nombre, descripcion:
+        const article = await models.Articulos.update({codigo: req.body.codigo, nombre: req.body.nombre, descripcion:
         req.body.descripcion }, { where: { id: req.body.id } });
         res.status(200).json(article);
 
@@ -54,7 +54,7 @@ exports.update = async (req, res, next) => {
 exports.activate = async (req, res, next) => {
     try {
         
-        const article = await models.Articulo.update({ estado: 1 }, { where: { id: req.body.id } });
+        const article = await models.Articulos.update({ estado: 1 }, { where: { id: req.body.id } });
         res.status(200).json(article);
     } catch (error) {
         res.status(500).send({
@@ -66,7 +66,7 @@ exports.activate = async (req, res, next) => {
 
 exports.deactivate = async (req, res, next) => {
     try {
-        const article = await models.Articulo.update({ estado: 0 }, { where: { id: req.body.id } });
+        const article = await models.Articulos.update({ estado: 0 }, { where: { id: req.body.id } });
         res.status(200).json(article);
     } catch (error) {
         res.status(500).send({
